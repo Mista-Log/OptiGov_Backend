cead00a7fc09ecb4505dac0f8159318d5c0e1ea2
Mista-Log <oloyedeibrahimsmile@gmail.com>
2025-07-05 07:11:45 +0100

    Logout view created

--- OptiGov/settings.py
+++ OptiGov/settings.py
@@ -130,10 +130,10 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
+    # 'ROTATE_REFRESH_TOKENS': True,
+    # 'BLACKLIST_AFTER_ROTATION': True,
 }
 
 
